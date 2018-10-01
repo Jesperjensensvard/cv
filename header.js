efc9863090d5fd11955321bf8ec3d9813dacd225
Jesper Jensen sverd <jesperjensensverd@Jespers-MacBook-Pro.local>
2018-10-01 10:03:19 +0200

    initial commit

---
+++ header.js
@@ -0,0 +1,19 @@
+
+function menuButtonClicked() {
+   var header = document.getElementById("header");
+   header.classList.toggle("fullscreen")
+
+ /*   if(header.className == "fullscreen") {
+       header.className = "";
+       console.log("stänger header");
+
+   }
+   else{
+    header.className= "fullscreen";
+    console.log("öppnar header")
+   }
+   */
+}
+
+
+
